6b155cd77a6b08e0fc55cc40a60534f23a6ba3ea
kristy <kristystallings@outlook.com>
2021-01-03 10:48:07 -0600

    updating siriusly api to springboot

---
+++ src/Services/Siriusly/src/main/java/com/KristyStallings/Siriusly/SiriuslyApplication.java
@@ -0,0 +1,13 @@
+package com.KristyStallings.Siriusly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiriuslyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiriuslyApplication.class, args);
+	}
+
+}
---
+++ src/Services/Siriusly/src/test/java/com/KristyStallings/Siriusly/SiriuslyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KristyStallings.Siriusly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiriuslyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
